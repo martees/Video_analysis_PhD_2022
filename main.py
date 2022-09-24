38700ab58b3ecd35be7c39917cd0f3aec8398331
martees <60579713+martees@users.noreply.github.com>
2022-09-24 11:38:36 +0200

    reads traj mat files into a dataframe, plots trajectories

--- main.py
+++ main.py
@@ -7,39 +7,58 @@ import numpy as np
 from scipy.io import loadmat  # this is the SciPy module that loads mat-files
 from datetime import datetime, date, time
 import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+def is_linux():  #returns true if you're using linux, otherwise false
+    try:
+        test = os.uname()
+        if test[0] == "Linux":
+            return True
+    except AttributeError:
+        return False
+
+def path_finding():
+    '''
+    Function to set the paths of the tracking outputs.
+    :return:
+    '''
+    #TODO Set these to a list of prefixes for bulk analysis, keep folder info as the variable name?
+    path_prefix_windows = "C:/Users/Asmar/Desktop/Thèse/2022_summer_videos/20220721T163616_StandardizedConditions_C5_CAM1_Tracking_Video/"
+    path_prefix_linux = "/home/admin/Desktop/Camera_setup_analysis/Tracking_Video/"
+
+    if is_linux():
+        path_prefix = path_prefix_linux
+    else:
+        path_prefix = path_prefix_windows
+
+    return path_prefix + "traj.mat"
 
 
-def mat_to_pandas(path_of_mat):
+def trajmat_to_pandas(path_of_mat):
     """
     Takes the path of a .mat object, and returns a pandas dataframe containing the same data
     """
-    mat = loadmat(path_of_mat)  # load mat-file
-    mdata = mat['measuredData']  # variable in mat file
-    mdtype = mdata.dtype  # dtypes of structures are "unsized objects"
-    # * SciPy reads in structures as structured NumPy arrays of dtype object
-    # * The size of the array is the size of the structure array, not the number
-    #   elements in any particular field. The shape defaults to 2-dimensional.
-    # * For convenience make a dictionary of the data using the names from dtypes
-    # * Since the structure has only one element, but is 2-D, index it at [0, 0]
-    ndata = {n: mdata[n][0, 0] for n in mdtype.names}
-    # Reconstruct the columns of the data table from just the time series
-    # Use the number of intervals to test if a field is a column or metadata
-    columns = [n for n, v in ndata.iteritems() if v.size == ndata['numIntervals']]
-    # now make a data frame, setting the time stamps as the index
-    return pd.DataFrame(np.concatenate([ndata[c] for c in columns], axis=1),
-                        index=[datetime(*ts) for ts in ndata['timestamps']], columns=columns)
+    # Loads .mat file into a dictionnary with meta info
+    trajmat = loadmat(path_of_mat) # the data is stored as a value for the key with the original table name ('traj' for traj.mat)
+    # Structure of traj.mat: traj.mat[0] = one line per worm, with their x,y positions at t_0
+    # So if you call traj.mat[:,0] you get all the positions of the first worm.
+    nb_of_worms = len(trajmat.get('traj')[0])
+    trajectories_worm_xy_time = [] #first list index is worm, second is x/t, third is time
+    for worm in range(nb_of_worms):
+        trajectories_worm_xy_time.append(pd.DataFrame(trajmat.get('traj')[:,worm]))
+    return trajectories_worm_xy_time
+
+def draw(trajectories):
+    """
+    Function that takes in a list of series of positions and draws them.
+    :param trajectory: list of series of (x,y) positions ([[x0,x1,x2...] [y0,y1,y2...])
+    :return: trajectory plot
+    """
+    for i_traj in range(len(trajectories)):
+        plt.plot(trajectories[i_traj][0],trajectories[i_traj][1])
+    plt.show()
+
+draw(trajmat_to_pandas(path_finding()))
 
 
-path = "/home/admin/Desktop/Camera_setup_analysis/"
-traj = mat_to_pandas(path + "Tracking_Video/traj.mat")
-vt_b = mat_to_pandas(path + "Tracking_Video/traj.mat")
-vt_background = mat_to_pandas(path + "Tracking_Video/vt_background.mat")
-vt_br = mat_to_pandas(path + "Tracking_Video/vt_br.mat")
-vt_mask = mat_to_pandas(path + "Tracking_Video/vt_mask.mat")
-vt = mat_to_pandas(path + "Tracking_Video/vt.mat")
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
