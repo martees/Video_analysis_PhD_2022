3e4d547dc6d9939e8f5f232f98a92d269b960837
martees <alicealasmar@yahoo.fr>
2022-10-14 20:16:37 +0200

    idk what im doing with git lmao

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ def is_linux():  #returns true if you're using linux, otherwise false
         return False
 
 def path_finding():
-    '''
-    Function to set the paths of the tracking outputs.
-    :return:
-    '''
+    """
+    Function that takes a path and returns the path of the traj.mat
+    """
     #TODO Set these to a list of prefixes for bulk analysis, keep folder info as the variable name?
     path_prefix_windows = "C:/Users/Asmar/Desktop/Thèse/2022_summer_videos/20220721T163616_StandardizedConditions_C5_CAM1_Tracking_Video/"
     path_prefix_linux = "/home/admin/Desktop/Camera_setup_analysis/Tracking_Video/"
@@ -59,6 +58,36 @@ def draw(trajectories):
         plt.plot(trajectories[i_traj][0],trajectories[i_traj][1])
     plt.show()
 
+def in_patch(position, patch, ):
+    """
+    returns true if position = [x,y] is inside the patch
+    tolerance is that the worm is still considered inside the patch when its center is sticking out but only a bit
+    """
+    center = patch[0]
+    radius = patch[1]
+    distance = sqrt((position[0]-center[0])**2 + (position[1]-center[1])**2)
+    return distance < radius + radial_tolerance
+
+def patch_visits(trajectory, list_of_patches):
+    """
+    returns a list [[d0,d1,...], [d0,d1,...],...] with one list per patch
+    each sublist contains the durations of visits to each patch
+    """
+    is_in_patch = np.ones(len(list_of_patches)) # Bool list to store where the worm is currently
+    list_of_durations = [list(i) for i in np.zeros((10,1), dtype='int')] # List with the right format, 0 for each patch
+
+    #We go through the whole trajectory
+    for t in range(len(trajectory)):
+        was_in_patch = is_in_patch # Keeping in memory where the worm was
+        for i_patch in list_of_patches:
+            is_in_patch[i_patch] = in_patch(trajectory[t], list_of_patches[i])
+
+
+
+
 draw(trajmat_to_pandas(path_finding()))
 
+# Parameters
+radial_tolerance = 0.1
+fake_patch = [[1400, 1200], 100]  # [[x,y], radius] with x y = position of the center
 
