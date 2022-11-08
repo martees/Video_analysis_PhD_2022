9902c56182a83c3b9707dbb6c4b9d5e5b440efe2
martees <60579713+martees@users.noreply.github.com>
2022-11-08 01:30:40 +0100

    added a dumb bool lmao

--- main.py
+++ main.py
@@ -38,16 +38,18 @@ def avg_duration_per_condition(result_table):
 # path = "C:/Users/Asmar/Desktop/Thèse/2022_summer_videos"
 path = "/home/admin/Desktop/Camera_setup_analysis/Results_minipatches_Nov2022_clean/"
 
-# Extracting data: only run this once in the beginning of your analysis!
-# The function looks for all "traj.csv" files in the indicated path (will look into subfolders)
-# and generate a result table from it, containing one line per worm and the following columns:
-# results_table["condition"] =
-# results_table["worm_id"] =
-# results_table["raw_visits"] = all visits of all patches
-# results_table["avg_visit_duration"] =
-# results_table["furthest_patch_distance"] =
-
-#gr.extract_and_save(path) #run this once, will save results under path+"results.csv"
+# Only run this once in the beginning of your analysis!
+# Extracting data, the function looks for all "traj.csv" files in the indicated path (will look into subfolders)
+# It will then generate a "results" table, with one line per worm, and these info:
+#         results_table["condition"] = condition written on the plate of the worm
+#         results_table["worm_id"] = number of the worm (100 times the file number + id attributed by tracking algorithm)
+#         results_table["raw_visits"] = list outputed by patch_visits_single_traj (see its description)
+#         results_table["avg_visit_duration"] = average visit duration of that worm
+#         results_table["furthest_patch_distance"] = furthest patch visited
+# Saves these results in a "results.csv" file in path, so no need to run this line every time!
+regenerate_data = False
+if regenerate_data:
+    gr.generate_and_save(path) #run this once, will save results under path+"results.csv"
 
 results = pd.read_csv(path+"results.csv") #run this to retrieve results
 condition_nb, average_per_condition, errorbars = avg_duration_per_condition(results)
