1322a2618f76e6afdd589c45e04f7a232c9f6c09
martees <alicealasmar@yahoo.fr>
2023-06-05 11:10:11 +0200

    added condition names

--- param.py
+++ param.py
@@ -4,3 +4,5 @@
 radial_tolerance = 0
 # Just a parameter to toggle extensive printing in the functions (for debugging purposes)
 verbose = False
+condition_names = ["close 0.2", "med 0.2", "far 0.2", "cluster 0.2", "close 0.5", "med 0.5", "far 0.5",
+                   "cluster 0.5", "med 1.25", "med 0.2+0.5", "med 0.5+1.25"]
