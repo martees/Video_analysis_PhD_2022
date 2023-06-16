4e70d7bfc945698d502ee2ae652b3217fda85d6f
martees <60579713+martees@users.noreply.github.com>
2023-06-16 19:44:58 +0200

    nothing

--- param.py
+++ param.py
@@ -1,5 +1,6 @@
 # General parameters
 
+# NOT IMPLEMENTED YET
 # Tolerance for the worm exiting a food patch, so when it's radius+tolerance away we still count it as being inside
 radial_tolerance = 0
 # Just a parameter to toggle extensive printing in the functions (for debugging purposes)
