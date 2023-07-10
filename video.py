3e2c24b386ae6f92b80a1463a90a5cb9d103de99
martees <alicealasmar@yahoo.fr>
2023-07-10 17:21:52 +0200

    debugged worm plotting and now it's great

--- video.py
+++ video.py
@@ -61,7 +61,7 @@ def show_frames(folder, first_frame):
 
     # Plot the background
     composite = plt.imread(folder[:-len('traj.csv')] + "composite_patches.tif")
-    top_ax.imshow(composite, origin="lower")
+    top_ax.imshow(composite)
 
     # Plot the patches
     fig = plt.gcf()
@@ -104,33 +104,15 @@ def show_frames(folder, first_frame):
     global curr_index
     curr_index = fd.load_index(folder, first_frame)
 
-    # Plot the worm
-    global worm_plot
-    worm_plot = top_ax.plot(pixels[curr_index][0], pixels[curr_index][1],
-                            color=plt.cm.Greys(np.linspace(0, 1, 256))[80], marker="o", linewidth=0)
-
-    # Plot the center of mass of the worm
+    # Initialize the plot objects
+    global worm_plot  # worm silhouette
     global center_of_mass_plot
-    center_of_mass_plot = top_ax.scatter(centers_of_mass["x"][curr_index], centers_of_mass["y"][curr_index], zorder=3, color="orange")
-
-    # Plot a line between the center of the patch and the center of the worm
-    global center_to_center_line
+    global center_to_center_line  # a line between the center of the patch and the center of the worm
+    worm_plot = top_ax.plot([], [], color=plt.cm.Greys(np.linspace(0, 1, 256))[80], marker="o", linewidth=0)
+    center_of_mass_plot = top_ax.scatter([], [], zorder=3, color="orange")
     center_to_center_line = top_ax.plot([], [], color="white")
-    if patch_list[curr_index] != -1:
-        curr_patch_center = patch_centers[patch_list[curr_index]]
-        curr_worm_pos = [centers_of_mass["x"][curr_index], centers_of_mass["y"][curr_index]]
-        center_to_center_line[0].set_data([curr_patch_center[0], curr_worm_pos[0]], [curr_patch_center[1], curr_worm_pos[1]])
 
-    # Put the frame number (different from frame index due to missing frames in the tracking)
-    top_ax.set_title("Frame: " + str(fd.load_frame(folder, curr_index)) + ", patch: " + str(patch_list[curr_index]))
-
-    # As a y-label, put distance of worm to center computed geometrically and spline value
-    if patch_list[curr_index] != -1
-        worm_to_center = np.sqrt((curr_patch_center[0] - curr_worm_pos[0])**2 - (curr_patch_center[1] - curr_worm_pos[1])**2)
-        angular_position = np.arctan2((curr_worm_pos[1] - curr_patch_center[1]), (curr_worm_pos[0] - curr_patch_center[0]))
-        distance_from_center = np.sqrt((curr_worm_pos[0] - curr_patch_center[0]) ** 2 + (curr_worm_pos[1] - curr_patch_center[1]) ** 2)
-        spline_value = gr.spline_value(angular_position, current_metadata["spline_breaks"], current_metadata["spline_coefs"])
-        top_ax.set_ylabel("Worm_distance")
+    update_frame(folder, curr_index, pixels, centers_of_mass, patch_list, patch_centers, current_metadata)
 
     # Make the plot scrollable
     def key_event(e):
@@ -141,7 +123,7 @@ def show_frames(folder, first_frame):
             curr_index = max(0, curr_index - 1)
         else:
             return
-        update_frame(folder, curr_index, pixels, centers_of_mass, patch_list, patch_centers)
+        update_frame(folder, curr_index, pixels, centers_of_mass, patch_list, patch_centers, current_metadata)
 
     fig.canvas.mpl_connect('scroll_event', key_event)
 
@@ -157,7 +139,7 @@ def show_frames(folder, first_frame):
         global curr_index
         global bottom_ax
         curr_index = int(frame_slider.val)
-        update_frame(folder, curr_index, pixels, centers_of_mass, patch_list, patch_centers, zoom_out=True)
+        update_frame(folder, curr_index, pixels, centers_of_mass, patch_list, patch_centers, current_metadata, zoom_out=True)
 
     # Slider function update call
     frame_slider.on_changed(slider_update)
@@ -165,7 +147,7 @@ def show_frames(folder, first_frame):
     plt.show()
 
 
-def update_frame(folder, index, pixels, centers_of_mass, patch_list, patch_centers, zoom_out=False):
+def update_frame(folder, index, pixels, centers_of_mass, patch_list, patch_centers, current_metadata, zoom_out=False):
     global worm_plot
     global center_of_mass_plot
     global center_to_center_line
@@ -175,15 +157,10 @@ def update_frame(folder, index, pixels, centers_of_mass, patch_list, patch_cente
     worm_plot[0].set_data([pixels[index][0]], [pixels[index][1]])
     center_of_mass_plot.set_offsets([centers_of_mass["x"][index], centers_of_mass["y"][index]])
 
-    curr_x = centers_of_mass["x"][curr_index]
-    curr_y = centers_of_mass["y"][curr_index]
+    curr_x = centers_of_mass["x"][index]
+    curr_y = centers_of_mass["y"][index]
     curr_patch = patch_list[index]
     top_ax.scatter(curr_x, curr_y, s=4)
-
-    if patch_list[curr_index] != -1:
-        curr_patch_center = patch_centers[curr_patch]
-        curr_worm_pos = [curr_x, curr_y]
-        center_to_center_line[0].set_data([curr_patch_center[0], curr_worm_pos[0]], [curr_patch_center[1], curr_worm_pos[1]])
 
     if zoom_out:
         top_ax.set_xlim(img_xmin, img_xmax)
@@ -191,14 +168,32 @@ def update_frame(folder, index, pixels, centers_of_mass, patch_list, patch_cente
     else:
         xmin, xmax, ymin, ymax = curr_x - 100, curr_x + 100, curr_y - 100, curr_y + 100
         top_ax.set_xlim(xmin, xmax)
-        top_ax.set_ylim(ymin, ymax)
+        top_ax.set_ylim(ymax, ymin)  # min and max values reversed because in our background image y-axis is reversed
 
-    top_ax.set_title("Frame: " + str(fd.load_frame(folder, index)) + ", patch: " + str(curr_patch))
+    # If worm is in a patch
+    if patch_list[curr_index] != -1:
+        # Draw a line between the patch center and the worm center of mass
+        curr_patch_center = patch_centers[curr_patch]
+        curr_worm_pos = [curr_x, curr_y]
+        center_to_center_line[0].set_data([curr_patch_center[0], curr_worm_pos[0]], [curr_patch_center[1], curr_worm_pos[1]])
+
+        # As a y-label, put distance of worm to center computed geometrically and spline value
+        worm_to_center = np.sqrt((curr_patch_center[0] - curr_worm_pos[0])**2 + (curr_patch_center[1] - curr_worm_pos[1])**2)
+        angular_position = np.arctan2((curr_worm_pos[1] - curr_patch_center[1]), (curr_worm_pos[0] - curr_patch_center[0]))
+        spline_value = gr.spline_value(angular_position, current_metadata["spline_breaks"][curr_patch], current_metadata["spline_coefs"][curr_patch])
+        # Plot spline value
+        top_ax.plot(patch_centers[curr_patch][0] + (spline_value * np.cos(angular_position)), patch_centers[curr_patch][1] + (spline_value * np.sin(angular_position)), marker="x", color="yellow")
+
+        top_ax.annotate(str(patch_centers[curr_patch][0])+", "+str(patch_centers[curr_patch][1]), [patch_centers[curr_patch][0], patch_centers[curr_patch][1]])
+
+        top_ax.set_xlabel("Worm_distance: "+str(worm_to_center)+", spline value: "+str(spline_value))
+
+    top_ax.set_title("Frame: " + str(fd.load_frame(folder, index)) + ", patch: " + str(curr_patch)+", worm xy: "+str(curr_x)+", "+str(curr_y))
 
     curr_fig = plt.gcf()
     curr_fig.canvas.draw()
 
 
 show_frames(
-    "/home/admin/Desktop/Camera_setup_analysis/Results_minipatches_20221108_clean_fp/20221011T111213_SmallPatches_C1-CAM3/traj.csv",
+    "/home/admin/Desktop/Camera_setup_analysis/Results_minipatches_20221108_clean_fp/20221014T101512_SmallPatches_C1-CAM1/traj.csv",
     612)
