380dd01e6834226bad7a80e36e53d6e2224654c5
martees <alicealasmar@yahoo.fr>
2023-07-13 01:18:52 +0200

    new controls accommodation

--- find_data.py
+++ find_data.py
@@ -18,21 +18,22 @@ def is_linux():  # returns true if you're using linux, otherwise false
          return False
 
 
-def path_finding_traj(path_prefix):
+def path_finding_traj(path_prefix, target_name="traj.csv", include_fake_control_folders=False):
     """
-    Function that takes a folder prefix and returns a list of paths of the traj.csv files present in that folder
-    WARNING: it will look through subfolders if necessary, so it might take a while if your folder is very big!
+    Function that takes a folder prefix and returns a list of paths of the "target_name" files present in that folder.
+    :include_fake_control_folders: if TRUE, the function will return all the paths. If FALSE, function will exclude
+                                   folders that have the word "control" in it.
     """
-    #These are now arguments of the function to avoid unnecessary hard coded mess
-    #path_prefix_windows = "C:/Users/Asmar/Desktop/Thèse/2022_summer_videos/20220721T163616_StandardizedConditions_C5_CAM1_Tracking_Video"
-    #path_prefix_linux = "/home/admin/Desktop/Camera_setup_analysis/Tracking_Video"
+    listofpaths = glob.glob(path_prefix + "/**/"+target_name, recursive=True)
 
-    listofpaths = glob.glob(path_prefix + "/**/traj.csv", recursive=True)
+    if not include_fake_control_folders:
+        listofpaths = [path for path in listofpaths if "control" not in path]
 
     if not is_linux():  # On windows the glob output uses \\ as separators so remove that
         listofpaths = [name.replace("\\",'/') for name in listofpaths]
 
-    print("Finished path finding")
+    if verbose:
+        print("Finished path finding")
     return listofpaths
 
 
@@ -81,71 +82,59 @@ def folder_to_metadata(path):
         "spline_coefs": coefficients of the polynomials for each inter-break interval
     :param path: a string with the path leading to the traj.csv whose metadata you want to retrieve (metadata should be in the same folder)
     """
-    metadata = pd.DataFrame() #where we'll put everything
+    # If the path contains the word control, which means it's a folder created during analysis to contain fake patches
+    if "control" in path:
+        metadata = pd.read_csv(path[:-len("traj.csv")]+"metadata.csv")
+        metadata["patch_centers"] = [json.loads(metadata["patch_centers"][i]) for i in range(len(metadata["patch_centers"]))]
+        metadata["spline_guides"] = [json.loads(metadata["spline_guides"][i]) for i in range(len(metadata["patch_centers"]))]
+        metadata["spline_breaks"] = [json.loads(metadata["spline_breaks"][i]) for i in range(len(metadata["patch_centers"]))]
+        metadata["spline_coefs"] = [json.loads(metadata["spline_coefs"][i]) for i in range(len(metadata["patch_centers"]))]
+        metadata["holes"] = [json.loads(metadata["holes"][i]) for i in range(len(metadata["patch_centers"]))]
 
-    # Finding the path of the other files
-    lentoremove = len('traj.csv')  # removes traj from the current path, to get to the parent folder
-    path_for_holes = path[:-lentoremove] + "holes.mat"
-    path_for_patches = path[:-lentoremove] + "foodpatches.mat"
-    path_for_patch_splines = path[:-lentoremove] + "foodpatches_new.mat"
+        return metadata
 
-    # Loadmat function loads .mat file into a dictionnary with meta info
-    # the data is stored as a value for the key with the original table name ('traj' for traj.mat)
-    holesmat = loadmat(path_for_holes)  # load holes in a dictionary using loadmat
-    patchesmat = loadmat(path_for_patches)  # load old patch objects (just centers and densities)
-    splinesmat = loadmat(path_for_patch_splines)  # load alfonso's patch objects (with spline info)
+    # Else if it's a normal folder that went through Alfonso's tracking pipeline (metadata stored in .mat files)
+    else:
+        metadata = pd.DataFrame() #where we'll put everything
 
-    # Extract patch objects
-    patch_objects = splinesmat.get("fp_struct")[0]
-    # In patch_object[0] are 8x2 lists of interpolation points that were used to extract the spline
-    # In patch_object[1] is a 1x2 list with the patch center coordinates
-    # In patch_object[2] is a matlab spline object stuck in two [[[]]], so we access it like that:
-    spline_guides = [patch_objects[i][0] for i in range(len(patch_objects))]
-    patch_centers = [patch_objects[i][1] for i in range(len(patch_objects))]
-    spline_objects = [patch_objects[i][2] for i in range(len(patch_objects))]
-    # In spline_objects[0] is useless strings
-    # In spline_objects[1] is the breaks which give the intervals for each spline equation
-    # In spline_objects[2] are the coefficients of the spline
+        # Finding the path of the other files
+        lentoremove = len('traj.csv')  # removes traj from the current path, to get to the parent folder
+        path_for_holes = path[:-lentoremove] + "holes.mat"
+        path_for_patches = path[:-lentoremove] + "foodpatches.mat"
+        path_for_patch_splines = path[:-lentoremove] + "foodpatches_new.mat"
 
-    # Extract the data into the dataframe
-    # holepositions = pd.DataFrame(holesmat.get('pointList')) # gets the holes positions
-    # condition_number = readcode(holepositions) #get the condition from that
+        # Loadmat function loads .mat file into a dictionnary with meta info
+        # the data is stored as a value for the key with the original table name ('traj' for traj.mat)
+        holesmat = loadmat(path_for_holes)  # load holes in a dictionary using loadmat
+        patchesmat = loadmat(path_for_patches)  # load old patch objects (just centers and densities)
+        splinesmat = loadmat(path_for_patch_splines)  # load alfonso's patch objects (with spline info)
 
-    metadata["patch_centers"] = [patch_centers[i][0] for i in range(len(patch_centers))]
-    metadata["spline_guides"] = [spline_guides[i].tolist() for i in range(len(spline_guides))]
-    metadata["spline_breaks"] = [list(spline_objects[i][0][0][1][0]) for i in range(len(spline_objects))]
-    metadata["spline_coefs"] = [[list(spline_objects[j][0][0][2][i]) for i in range(len(spline_objects[j][0][0][2]))] for j in range(len(spline_objects))]
+        # Extract patch objects
+        patch_objects = splinesmat.get("fp_struct")[0]
+        # In patch_object[0] are 8x2 lists of interpolation points that were used to extract the spline
+        # In patch_object[1] is a 1x2 list with the patch center coordinates
+        # In patch_object[2] is a matlab spline object stuck in two [[[]]], so we access it like that:
+        spline_guides = [patch_objects[i][0] for i in range(len(patch_objects))]
+        patch_centers = [patch_objects[i][1] for i in range(len(patch_objects))]
+        spline_objects = [patch_objects[i][2] for i in range(len(patch_objects))]
+        # In spline_objects[0] is useless strings
+        # In spline_objects[1] is the breaks which give the intervals for each spline equation
+        # In spline_objects[2] are the coefficients of the spline
 
-    metadata["patch_densities"] = list(patchesmat.get("densities_patches"))
-    metadata["condition"] = list(patchesmat.get("num_condition"))[0][0]
-    metadata["holes"] = [[hole for hole in holesmat.get("pointList").tolist() if hole[2] == 1] for i_patch in range(len(metadata["patch_centers"]))]
+        # Extract the data into the dataframe
+        # holepositions = pd.DataFrame(holesmat.get('pointList')) # gets the holes positions
+        # condition_number = readcode(holepositions) #get the condition from that
+
+        metadata["patch_centers"] = [patch_centers[i][0] for i in range(len(patch_centers))]
+        metadata["spline_guides"] = [spline_guides[i].tolist() for i in range(len(spline_guides))]
+        metadata["spline_breaks"] = [list(spline_objects[i][0][0][1][0]) for i in range(len(spline_objects))]
+        metadata["spline_coefs"] = [[list(spline_objects[j][0][0][2][i]) for i in range(len(spline_objects[j][0][0][2]))] for j in range(len(spline_objects))]
+
+        metadata["patch_densities"] = list(patchesmat.get("densities_patches"))
+        metadata["condition"] = list(patchesmat.get("num_condition"))[0][0]
+        metadata["holes"] = [[hole[0:2] for hole in holesmat.get("pointList").tolist() if hole[2] == 2] for i_patch in range(len(metadata["patch_centers"]))]
 
     return metadata
-
-
-def perfect_square_parameters(x1, y1, x2, y2, x3, y3, x4, y4):
-    # Create numpy arrays for the points
-    points = np.array([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
-
-    # Calculate the centroid of the points
-    centroid = np.mean(points, axis=0)
-
-    # Translate the points so that the centroid is at the origin
-    translated_points = points - centroid
-
-    # Perform singular value decomposition (SVD) on the translated points
-    _, _, vh = np.linalg.svd(translated_points)
-
-    # Extract the rotation matrix from the right singular vectors
-    rotation_matrix = vh.T @ vh
-
-    # Calculate the scaling factor as the square root of the largest eigenvalue of the rotation matrix
-    scaling_factor = np.sqrt(np.max(np.linalg.eigvals(rotation_matrix)))
-
-    # Calculate the rotation angle from the rotation matrix
-    rotation_angle = np.arctan2(rotation_matrix[1, 0], rotation_matrix[0, 0])
-
-    return scaling_factor, rotation_angle
 
 
 def load_silhouette(path):
@@ -170,7 +159,7 @@ def load_silhouette(path):
 
 
 def return_folders_condition_list(full_folder_list, condition_list):
-    if condition_list is int:
+    if type(condition_list) is int:
         condition_list = [condition_list]
     condition_folders = []
     for folder in full_folder_list:
@@ -239,3 +228,22 @@ def control_folders(path, condition_names):
     for condition_name in condition_names:
         path_list.append(path + "_control_" + condition_name)
     return path_list
+
+
+def load_image(folder, image_name):
+    """
+    Take a folder path ending in /traj.csv, and return the path of the corresponding composite_patches.tif / background.tif.
+    In non-control conditions they are just in the same folder so it's easy, but for control sub-folders, you have to
+    go look into the parent folder.
+    image_name = "composite_patches.tif" or "background.tif"
+    """
+    folder = folder[:-len("traj.csv")]  # in any case, remove traj.csv
+    # For non-control experiments it's just in the same folder
+    if "control" not in folder:
+        return folder + image_name
+    # For control experiments, it's in the parent folder
+    else:
+        folder = folder[:-1]  # remove end "/" (leftover after we only removed "traj.csv"
+        parse_path = folder.split("/")  # get a list of the folders along the path
+        return folder[:-len(parse_path[-1])] + image_name  # remove last folder to get to parent, then add composite
+
