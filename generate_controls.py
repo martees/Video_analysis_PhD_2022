3823bc680c96b845488d5454d8622a0c8c4b741d
martees <alicealasmar@yahoo.fr>
2023-07-19 21:23:01 +0200

    fixed metadata saving and pixel to mm conversion

--- generate_controls.py
+++ generate_controls.py
@@ -9,7 +9,7 @@ import scipy
 import parameters
 # My code
 import find_data as fd
-import plots
+# import plots
 
 # Originally, our controls are saved in some folder. In order to have one control per inter-patch distance, we create
 #     subfolders inside of those original folders, containing the name of the corresponding distance. Eg : inside the folder
@@ -26,7 +26,7 @@ def rename_original_control_traj(path):
     This should only do anything if you have just imported new folders.
     """
     # Full list of paths for the traj.csv files that can be found in the arborescence starting in path
-    folder_list = fd.path_finding_traj(path, include_fake_control_folders=False)
+    folder_list = fd.path_finding_traj(path, target_name="traj.csv", include_fake_control_folders=False)
     # Select folders that correspond to a control condition (11)
     folder_list = fd.return_folders_condition_list(folder_list, 11)
     # Rename them allllll
@@ -34,7 +34,7 @@ def rename_original_control_traj(path):
         os.rename(folder, folder[:-len("traj.csv")]+"traj_parent.csv")
 
 
-def generate_controls(path, show=True):
+def generate_controls(path, show=False):
     """
     Takes a path prefix, finds the files of all control conditions inside.
     Will create subfolders (named parentfolder_control_close, parentfolder_control_med, etc.) each containing:
@@ -45,7 +45,7 @@ def generate_controls(path, show=True):
     print("Starting to generate controls...")
     # Rename any (new) control traj.csv into traj_control.csv
     rename_original_control_traj(path)
-    # Full list of paths for the traj_control.csv files that can be found in the arborescence starting in path
+    # Full list of paths for the traj_parent.csv files that can be found in the arborescence starting in path
     folder_list = fd.path_finding_traj(path, target_name="traj_parent.csv", include_fake_control_folders=False)
     # For all control folders
     for i_folder in range(len(folder_list)):
@@ -62,14 +62,16 @@ def generate_controls(path, show=True):
                 os.mkdir(current_control_folder)
             # Make the metadata.csv files
             current_distance_condition = current_control_folder.split("_")[-1]  # eg "control_med" => "med" will be the last element of "_" split
+
             metadata, source_folder = return_control_metadata(path, folder, current_distance_condition)
             metadata.to_csv(current_control_folder+"/metadata.csv")
+
             # Check if there is a traj.csv file in the current_control_folder, otherwise copy it from parent
             # Do this AFTER creating metadata otherwise find_data functions can find a traj.csv file with no metadata in the folder (and get pissed)
             if not os.path.isfile(current_control_folder + "/traj.csv"):
                 # Copy the traj.csv from folder into current_control_folder
                 # (all folder names have /traj.csv in the end, but not current_control_folder) (output of fd.control_folders)
-                shutil.copy(folder, current_control_folder)
+                shutil.copy(folder, current_control_folder + "/traj.csv")
                 # folder_without_traj = folder[:-len("traj.csv")]
                 # shutil.copy(folder_without_traj+"composite.tif", current_control_folder)
                 # shutil.copy(folder_without_traj+"background.tif", current_control_folder)
@@ -93,8 +95,8 @@ def return_control_metadata(path, parent_folder, distance):
         - Converts that to the current reference points, and changes condition
     """
 
+    # Find the folders of the experiments with the same distance between the patches, without controls because they are crappy
     all_folders = fd.path_finding_traj(path, include_fake_control_folders=False)
-    # Find the folders of the experiments with the same distance between the patches
     same_distance_folders = fd.return_folders_condition_list(all_folders, parameters.name_to_nb_list[distance])
     the_chosen_one = random.choice(same_distance_folders)
     # Load patch information for source folder
@@ -110,25 +112,34 @@ def return_control_metadata(path, parent_folder, distance):
         source_xy_holes = source_folder_metadata["holes"][0]
         source_reference_points = ReferencePoints.ReferencePoints(source_xy_holes)
 
+    print("the chosen one = ", the_chosen_one)
+
     # Load holes info from parent folder of the current sub-folder
     parent_folder_metadata = fd.folder_to_metadata(parent_folder)
-    # Load patch_centers from source folder
+    # Load patch_centers from source folder, and convert them to mm
     patch_centers = source_folder_metadata["patch_centers"]
+    patch_centers = source_reference_points.pixel_to_mm([patch_centers[i].tolist() for i in range(len(patch_centers))])
     # We load the holes of the parent_folder (the control_folder traj.csv is a copy from the parent_folder's, so same ref)
     target_xy_holes = parent_folder_metadata["holes"][0]
     # Convert it to a ReferencePoints object and use one of the class methods to convert patch centers to this reference
     target_reference_points = ReferencePoints.ReferencePoints(target_xy_holes)
     patch_centers = target_reference_points.mm_to_pixel([patch_centers[i].tolist() for i in range(len(patch_centers))])
 
+    # Convert the spline guides to mm in first reference and back to pixels in new reference system
+    spline_guides = source_folder_metadata["spline_guides"]
+    spline_guides = [source_reference_points.pixel_to_mm(spline_guides[i]) for i in range(len(spline_guides))]
+    spline_guides = [target_reference_points.mm_to_pixel(spline_guides[i]) for i in range(len(spline_guides))]
+
     # Store it all in a metadata dataframe
     control_metadata = pd.DataFrame()
     control_metadata["patch_centers"] = patch_centers.tolist()
     control_metadata["holes"] = [target_xy_holes for _ in range(len(patch_centers))]
-    control_metadata["spline_guides"] = list(source_folder_metadata["spline_guides"])
+    control_metadata["spline_guides"] = list(spline_guides)
     control_metadata["spline_breaks"] = list(source_folder_metadata["spline_breaks"])
     control_metadata["spline_coefs"] = list(source_folder_metadata["spline_coefs"])
-    control_metadata["patch_densities"] = [0 for i in range(len(control_metadata["patch_centers"]))]
+    control_metadata["patch_densities"] = [0 for _ in range(len(control_metadata["patch_centers"]))]
     control_metadata["condition"] = parameters.name_to_nb[distance+" 0"]
+    control_metadata["folder_from_which_the_patches_come"] = [the_chosen_one for _ in range(len(control_metadata["patch_centers"]))]
     # metadata["patch_centers"] = [patch_centers[i][0] for i in range(len(patch_centers))]
     # metadata["spline_guides"] = [spline_guides[i].tolist() for i in range(len(spline_guides))]
     # metadata["spline_breaks"] = [list(spline_objects[i][0][0][1][0]) for i in range(len(spline_objects))]
@@ -137,3 +148,6 @@ def return_control_metadata(path, parent_folder, distance):
     # metadata["condition"]
 
     return control_metadata, the_chosen_one
+
+
+#generate_controls("/home/admin/Desktop/Camera_setup_analysis/Results_minipatches_20221108_clean_fp/", show=True)
