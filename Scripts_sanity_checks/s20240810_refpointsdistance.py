1ddd8f532f5fee48a71323ab7addfc8b84b9b2ee
martees <60579713+martees@users.noreply.github.com>
2024-10-25 01:32:05 +0200

    added square fitting to align reference points, and mm conversion factor distribution

--- Scripts_sanity_checks/s20240810_refpointsdistance.py
+++ Scripts_sanity_checks/s20240810_refpointsdistance.py
@@ -13,9 +13,33 @@ from Scripts_analysis import s20240605_global_presence_heatmaps as heatmap_scrip
 import find_data as fd
 from Parameters import parameters as param
 
+
+def rotate_points(points, angle):
+    """
+    Rotates the points counter-clockwise by the given angle (in radians).
+    """
+    rotation_matrix = np.array([[np.cos(angle), -np.sin(angle)],
+                                [np.sin(angle), np.cos(angle)]])
+    return np.dot(points, rotation_matrix.T)
+
+
+def optimal_rotation(points, reference_square):
+    """
+    Finds the optimal rotation angle to align points with a reference square.
+    """
+    def loss_function(angle):
+        # Rotate points by the given angle and compute the error with the reference square
+        rotated_points = rotate_points(points, angle)
+        return np.sum(np.linalg.norm(rotated_points - reference_square, axis=1))
+
+    # Optimize the rotation by minimizing the loss function
+    from scipy.optimize import minimize
+    result = minimize(lambda x: loss_function(x[0]), [0.0], bounds=[(-np.pi, np.pi)])
+    return rotate_points(points, result.x[0])
+
+
 path = gen.generate("")
 results = dt.fread(path + "clean_results.csv")
-trajectories = dt.fread(path + "clean_trajectories.csv")
 full_list_of_folders = results[:, "folder"].to_list()[0]
 print("Finished loading tables!")
 
@@ -28,15 +52,16 @@ distances_each_condition_top = []
 distances_each_condition_left = []
 distances_each_condition_right = []
 distances_each_condition_bottom = []
+pixel_to_mm_ratios = []
 
 heatmap_points = np.zeros((1847, 1847))
+plate_center = [1847/2, 1847/2]
 
 condition_names = []
 condition_colors = []
 for i_condition, condition in enumerate(all_conditions_list):
-    if i_condition % 3 == 0:
-        print(">>>>>> Condition ", i_condition, " / ", len(all_conditions_list))
-    # Compute average radius from a few plates of this condition
+    print(">>>>>> Condition ", i_condition, " / ", len(all_conditions_list))
+    # Compute average distance from a few plates of this condition
     plates_this_condition = fd.return_folders_condition_list(full_list_of_folders, condition)
     for i_plate, plate in enumerate(plates_this_condition):
         plate_metadata = fd.folder_to_metadata(plate)
@@ -56,15 +81,29 @@ for i_condition, condition in enumerate(all_conditions_list):
             distances_each_condition_left.append(left_dist)
             distances_each_condition_right.append(right_dist)
             distances_each_condition_bottom.append(bottom_dist)
-        heatmap_points[int(point1[1]), int(point1[0])] += 1
-        heatmap_points[int(point2[1]), int(point2[0])] += 1
-        heatmap_points[int(point3[1]), int(point3[0])] += 1
-        heatmap_points[int(point4[1]), int(point4[0])] += 1
+            pixel_to_mm_ratios += [32 / top_dist, 32 / left_dist, 32 / right_dist, 32 / bottom_dist]
+
+            # Then center them to the center of the plate
+            points_array = np.array([point1, point2, point3, point4])
+            x_centroid_shift = plate_center[0] - np.mean(points_array[:, 0])
+            y_centroid_shift = plate_center[1] - np.mean(points_array[:, 1])
+            points_array[:, 0] += x_centroid_shift
+            points_array[:, 1] += y_centroid_shift
+
+            # Then rotate them to minimize distance with a perfect square
+            perfect_square = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+            points_array = optimal_rotation(points_array, perfect_square)
+
+            # Add them to the heatmap
+            heatmap_points[int(points_array[0, 1]), int(points_array[0, 1])] += 1
+            heatmap_points[int(points_array[1, 1]), int(points_array[1, 1])] += 1
+            heatmap_points[int(points_array[2, 1]), int(points_array[2, 1])] += 1
+            heatmap_points[int(points_array[3, 1]), int(points_array[3, 1])] += 1
 
     condition_names.append(param.nb_to_name[condition])
     condition_colors.append(param.name_to_color[param.nb_to_name[condition]])
 
-fig, [ax0, ax1] = plt.subplots(1, 2)
+fig, [ax0, ax1, ax2] = plt.subplots(1, 3)
 
 # Heatmap of the points
 ax0.imshow(heatmap_points, cmap="hot", vmax=0.1)
@@ -75,8 +114,11 @@ ax1.boxplot([distances_each_condition_left, distances_each_condition_top, distan
 ax1.set_xticks([1, 2, 3, 4], ["Left", "Top", "Right", "Bottom"])
 ax1.set_title("Reference point distance for each edge")
 
+# Pixel to mm ratio histogram
+ax2.hist(pixel_to_mm_ratios, 100)
 all_distances = distances_each_condition_top + distances_each_condition_bottom + distances_each_condition_left + distances_each_condition_right
-print("You can copy this number in Parameters/parameters.py, for the variable one_pixel_in_mm: ", np.mean(all_distances))
+print("You can copy this number in Parameters/parameters.py, for the variable one_pixel_in_mm: ", 32 / np.mean(all_distances))
+ax2.set_title("Pixel to mm conversion distribution for all plates, avg: "+str(32 / np.mean(all_distances)))
 
 plt.show()
 
