c1c6dd4e62757fdf24e9ced8236697b3b5e6f0f3
martees <alicealasmar@yahoo.fr>
2024-10-26 19:53:49 +0200

    added nice legends and conversion to mm and plotting the hist of radius here

--- Scripts_sanity_checks/s20240810_interpatch_distance.py
+++ Scripts_sanity_checks/s20240810_interpatch_distance.py
@@ -12,17 +12,20 @@ from Scripts_analysis import s20240605_global_presence_heatmaps as heatmap_scrip
 from Scripts_models import s20240626_transitionmatrix as transition_script
 import find_data as fd
 from Parameters import parameters as param
+from Parameters import custom_legends
 
 
 def generate_patch_distances():
     path = gen.generate("")
     results = dt.fread(path + "clean_results.csv")
-    trajectories = dt.fread(path + "clean_trajectories.csv")
     full_list_of_folders = results[:, "folder"].to_list()[0]
     full_cond_list = list(param.nb_to_name.keys())
     print("Finished loading tables!")
 
-    distances = ["close", "med", "far", "superfar", "cluster"]
+    # Also show radius distribution
+    heatmap_script.generate_average_patch_radius_each_condition(gen.generate(""), full_list_of_folders)
+
+    distances = ["close", "med", "far", "superfar"]
     nearest_neighbors_each_cond = transition_script.find_nearest_neighbors(path, full_list_of_folders, full_cond_list)
     # Lists to fill in following loop
     list_nearest_neighbor_distances = [[] for _ in range(len(distances))]
@@ -44,18 +47,64 @@ def generate_patch_distances():
                 current_patch_xy = patch_centers[i_patch]
                 for j_neighbor in np.where(nearest_neighbors_each_cond[condition][i_patch] == 1)[0]:
                     neighbor_xy = patch_centers[j_neighbor]
-                    list_nearest_neighbor_distances[i_distance].append(ana.distance(current_patch_xy, neighbor_xy))
+                    list_nearest_neighbor_distances[i_distance].append(ana.distance(current_patch_xy, neighbor_xy)*param.one_pixel_in_mm)
     print("Finished retrieving neighbor distances!")
 
-    boxplot = plt.boxplot(list_nearest_neighbor_distances, patch_artist=True, labels=distances, bootstrap=1000)
+    boxplot = plt.boxplot(list_nearest_neighbor_distances, patch_artist=True, bootstrap=1000)
     colors = [param.name_to_color[dist] for dist in distances]
     # Fill with colors
     for patch, color in zip(boxplot['boxes'], colors):
         patch.set_facecolor(color)
-    plt.title("Average distance to the closest neighbor")
-    plt.yscale("log")
-    plt.xticks(rotation=45)
+    plt.title("Average distance to the closest neighbor (mm)", fontsize=20)
+    plt.ylabel("Average distance to the closest neighbor (mm)", fontsize=16)
+    #plt.yscale("log")
+
+    # # Custom x tick images with icon for the distance
+    # ax = plt.gca()
+    # ax.set_xticks([])
+    # for i_distance, distance in enumerate(distances):
+    #     from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
+    #
+    #     # Image to use
+    #     arr_img = plt.imread(
+    #         "/home/admin/Desktop/Camera_setup_analysis/Video_analysis/Parameters/icon_" + distance + '.png')
+    #
+    #     # Image box to draw it!
+    #     imagebox = OffsetImage(arr_img, zoom=0.5)
+    #     imagebox.image.axes = ax
+    #
+    #     x_annotation_box = AnnotationBbox(imagebox, (i_distance + 1, 0),
+    #                                       xybox=(0, -8),
+    #                                       # that's the shift that the image will have compared to (i, 0)
+    #                                       xycoords=("data", "axes fraction"),
+    #                                       boxcoords="offset points",
+    #                                       box_alignment=(.5, 1),
+    #                                       bboxprops={"edgecolor": "none"})
+    #
+    #     ax.add_artist(x_annotation_box)
+
+    # Horizontal lines with the average
+    ax = plt.gca()
+    lines = []
+    for i_distance, distance in enumerate(distances):
+        average_dist = np.mean(list_nearest_neighbor_distances[i_distance])
+        line = ax.axhline([average_dist], xmin=-1, xmax=10, linestyle="dashed", color=param.name_to_color[distance], label=distance, linewidth=2)
+        lines.append(line)
+    # Custom legend with doodles as labels
+    lines = [lines[len(lines) - i] for i in range(1, len(lines) + 1)]  # invert it for nicer order in legend
+    distances = [distances[len(distances) - i] for i in range(1, len(distances) + 1)]  # invert it for nicer order in legend
+    plt.legend(lines, ["" for _ in range(len(lines))],
+               handler_map={lines[i]: custom_legends.HandlerLineImage(
+                   "icon_" + distances[i] + ".png") for i in
+                   range(len(lines))},
+               handlelength=1.6, labelspacing=0.0, fontsize=50, borderpad=0.10, loc=2,
+               handletextpad=0.05, borderaxespad=0.15)
+
     plt.show()
 
     data_for_saving = pd.DataFrame({"distance": distances, "interpatch_distance": [np.mean(d) for d in list_nearest_neighbor_distances]})
     data_for_saving.to_csv(path + "interpatch_distance.csv")
+
+
+
+generate_patch_distances()
