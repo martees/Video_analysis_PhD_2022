9cad2cb7406515f8f0b8b1c19cf3a8122689bd5c
martees <60579713+martees@users.noreply.github.com>
2025-02-13 19:16:50 +0900

    added a safety net in case you run the code twice by mistake (it used to basically destroy your table oops)

--- Generating_data_tables/converting_folder_names.py
+++ Generating_data_tables/converting_folder_names.py
@@ -8,24 +8,24 @@ from Generating_data_tables import main as gen
 
 # CODE TO RUN WHEN ON WINDOWS WITH RESULTS PRODUCED IN LINUX
 def linux_to_windows():
-    linux_path = gen.generate("", shorten_traj=True, modeled_data=True, force_linux=True)
-    windows_path = gen.generate("", shorten_traj=True, modeled_data=True)
+    linux_path = gen.generate("", shorten_traj=False, modeled_data=False, force_linux=True)
+    windows_path = gen.generate("", shorten_traj=False, modeled_data=False)
     results = pd.read_csv(windows_path + "clean_results.csv")  # the results are saved in the windows path
     for i_folder in range(len(results["folder"])):
         current_folder = results["folder"][i_folder]
-        if str(current_folder) != "nan":
+        if str(current_folder) != "nan" and linux_path in current_folder:
             results.loc[i_folder, "folder"] = windows_path + current_folder[len(linux_path):]
     results.to_csv(windows_path + "clean_results.csv")
 
 
 # CODE TO RUN WHEN ON LINUX WITH RESULTS PRODUCED IN WINDOWS
 def windows_to_linux():
-    linux_path = gen.generate("", shorten_traj=True)
-    windows_path = gen.generate("", shorten_traj=True, force_windows=True)
+    linux_path = gen.generate("", shorten_traj=False)
+    windows_path = gen.generate("", shorten_traj=False, force_windows=True)
     results = pd.read_csv(linux_path + "clean_results.csv")  # the results are saved in the linux path
     for i_folder in range(len(results["folder"])):
         current_folder = results["folder"][i_folder]
-        if str(current_folder) != "nan":
+        if str(current_folder) != "nan" and windows_path in current_folder:
             results.loc[i_folder, "folder"] = linux_path + current_folder[len(windows_path):]
     results.to_csv(linux_path + "clean_results.csv")
 
@@ -45,3 +45,6 @@ def switch_path(custom_path):
                 path_elements = str(current_folder).split("/")
                 results.loc[i_folder, "folder"] = custom_path + path_elements[-3] + "/" + path_elements[-2] + "/traj.csv"
     results.to_csv(custom_path + "clean_results.csv")
+
+
+linux_to_windows()
