7b6b1040ed45167f159543c09ab8ab7d0743871c
martees <asmar.contact@yahoo.com>
2025-03-31 12:36:12 +0900

    minor tweaks (bins in the middle, axis conversion etc)

--- Scripts_analysis/s20241022_visit_duration_vs_time_in_patch.py
+++ Scripts_analysis/s20241022_visit_duration_vs_time_in_patch.py
@@ -25,16 +25,16 @@ def add_visit_vs_time_in_patch_1_folder(visit_list, bin_list, values_each_bin):
     i_bin_each_patch = [0 for _ in range(52)]
     current_bin_each_patch = [bin_list[0] for _ in range(52)]
     for i_visit, visit in enumerate(visit_list):
-        current_patch = visit[2]
-        print("TEMPORARY FIX: removing negative visits")
-        if visit[1] > visit[0]:
-            current_visit_duration = visit[1] - visit[0]
-            while total_time_each_patch[current_patch] > current_bin_each_patch[current_patch] and i_bin_each_patch[
-                current_patch] < len(bin_list) - 1:
-                i_bin_each_patch[current_patch] += 1
-                current_bin_each_patch[current_patch] = bin_list[i_bin_each_patch[current_patch]]
-            values_each_bin[i_bin_each_patch[current_patch]].append(current_visit_duration)
-            total_time_each_patch[current_patch] += current_visit_duration
+        current_patch = int(visit[2])
+        if visit[1] < visit[0]:
+            print("NEGATIVE VISIT AAAAAAA")
+        current_visit_duration = visit[1] - visit[0]
+        while total_time_each_patch[current_patch] > current_bin_each_patch[current_patch] and i_bin_each_patch[
+            current_patch] < len(bin_list) - 1:
+            i_bin_each_patch[current_patch] += 1
+            current_bin_each_patch[current_patch] = bin_list[i_bin_each_patch[current_patch]]
+        values_each_bin[i_bin_each_patch[current_patch]].append(current_visit_duration)
+        total_time_each_patch[current_patch] += current_visit_duration
     return values_each_bin
 
 
@@ -95,16 +95,22 @@ def plot_visit_duration_vs_time_in_patch(results, curve_list, bin_list, min_nb_e
         else:
             color = color_of_condition
             label = curve
-        plt.plot(np.array(bin_with_values)/3600, np.array(average_visit_length_each_bin)/3600,
+
+        # Set the bins to be in the middle instead of right edge of bin
+        bin_with_values = [0] + bin_with_values
+        bin_with_values = [(bin_with_values[i-1]+bin_with_values[i])/2 for i in range(1, len(bin_with_values))]
+
+        plt.plot(np.array(bin_with_values)/3600, np.array(average_visit_length_each_bin)/60,
                  color=color, label=label, linewidth=4)
-        plt.errorbar(np.array(bin_with_values)/3600, np.array(average_visit_length_each_bin)/3600,
-                     [np.array(error_inf_visit_length_each_bin)/3600, np.array(error_sup_visit_length_each_bin)/3600],
+        plt.errorbar(np.array(bin_with_values)/3600, np.array(average_visit_length_each_bin)/60,
+                     [np.array(error_inf_visit_length_each_bin)/60, np.array(error_sup_visit_length_each_bin)/60],
                      color=color, capsize=5)
 
     plt.yscale("log")
+    # plt.xscale("log")
     plt.title(str(curve_list), fontsize=20)
     #plt.xscale("log")
-    plt.ylabel("Average visit duration (hours)", fontsize=16)
+    plt.ylabel("Average visit duration (minutes)", fontsize=16)
     plt.xlabel("Time spent in patch before this visit (hours)", fontsize=16)
     plt.legend(fontsize=14)
     plt.show()
@@ -112,14 +118,14 @@ def plot_visit_duration_vs_time_in_patch(results, curve_list, bin_list, min_nb_e
 
 results_path = gen.generate("", shorten_traj=False)
 clean_results = pd.read_csv(results_path + "clean_results.csv")
-bins = [0, 400, 1000, 1600, 2000, 4000, 6000, 8000, 12000]
+bins = [100, 400, 1000, 1600, 2600, 4000, 6000, 8000, 12000]
 
-plot_visit_duration_vs_time_in_patch(clean_results, ["med 0", "med 0.2", "med 0.5", "med 1.25"], bins, 20, only_show_density=True)
+# plot_visit_duration_vs_time_in_patch(clean_results, ["med 0", "med 0.2", "med 0.5", "med 1.25"], bins, 20, only_show_density=True)
 
-plot_visit_duration_vs_time_in_patch(clean_results, ["close 0", "close 0.2", "close 0.5", "close 1.25"], bins, 20)
-plot_visit_duration_vs_time_in_patch(clean_results, ["med 0", "med 0.2", "med 0.5", "med 1.25"], bins, 20)
-plot_visit_duration_vs_time_in_patch(clean_results, ["far 0", "far 0.2", "far 0.5", "far 1.25"], bins, 20)
-plot_visit_duration_vs_time_in_patch(clean_results, ["superfar 0", "superfar 0.2", "superfar 0.5", "superfar 1.25"], bins, 20)
+# plot_visit_duration_vs_time_in_patch(clean_results, ["close 0", "close 0.2", "close 0.5", "close 1.25"], bins, 20)
+# plot_visit_duration_vs_time_in_patch(clean_results, ["med 0", "med 0.2", "med 0.5", "med 1.25"], bins, 20)
+# plot_visit_duration_vs_time_in_patch(clean_results, ["far 0", "far 0.2", "far 0.5", "far 1.25"], bins, 20)
+# plot_visit_duration_vs_time_in_patch(clean_results, ["superfar 0", "superfar 0.2", "superfar 0.5", "superfar 1.25"], bins, 20)
 
 plot_visit_duration_vs_time_in_patch(clean_results, ["close 0", "med 0", "far 0", "superfar 0"], bins, 20)
 plot_visit_duration_vs_time_in_patch(clean_results, ["close 0.2", "med 0.2", "far 0.2", "superfar 0.2"], bins, 20)
