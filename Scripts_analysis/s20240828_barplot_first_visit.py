1f29c28ad42ad3c0b6c792e1365fe1003bb57b2c
martees <asmar.contact@yahoo.com>
2025-03-31 12:26:37 +0900

    Fixed censorship handling (does not take second visit if first was censored)

--- Scripts_analysis/s20240828_barplot_first_visit.py
+++ Scripts_analysis/s20240828_barplot_first_visit.py
@@ -1,29 +1,59 @@
 import pandas as pd
 import numpy as np
+import copy
 import matplotlib.pyplot as plt
 import find_data as fd
 from Parameters import parameters as param
 from Generating_data_tables import main as gen
+from Generating_data_tables import generate_results as gr
 import analysis as ana
 
 
-def bar_plot_first_visit_each_patch(results, condition_list, is_plot=True, remove_censored_events=False,
+def bar_plot_first_visit_each_patch(results, condition_list, is_plot=True,
+                                    exclude_censored_events=False,
+                                    ignore_subsequent_uncensored_events=True,
                                     only_first_visited_patch=False, soft_cut=False, hard_cut=False,
                                     visits_longer_than=0):
     """
     Function that plots a histogram with the average length of first visit to each food patch in each condition in condition_list.
+    If "exclude_censored_events" is True, then censored events will be removed from the visit lists before analysis.
+    The "ignore_subsequent_uncensored_events" is only relevant when exclude_censored_events is also True.
+        If "ignore_subsequent_uncensored_events" is False, then the analysis is made on the first uncensored event of
+                    the patch (so in most cases, it means analyzing the second visit when the first is censored)
+        If "ignore_subsequent_uncensored_events" is True, then the analysis will ignore patches when the first visit
+                    was censored. In our dataset we also have a list named "visits_to_uncensored_patches", but in this
+                    case this is not suitable because it excludes patches that have *any* censored events, and we are
+                    just interested on whether their first visit is censored.
     """
     avg_value_each_condition = [0 for _ in range(len(condition_list))]
     errors_inf_each_condition = [0 for _ in range(len(condition_list))]
     errors_sup_each_condition = [0 for _ in range(len(condition_list))]
     avg_value_each_condition_each_plate = [[] for _ in range(len(condition_list))]
-    plate_list = results["folder"]
+    plate_list = fd.return_folders_condition_list(results["folder"], condition_list)
     for i_plate, plate in enumerate(plate_list):
         current_plate = results[results["folder"] == plate].reset_index()
         condition = current_plate["condition"][0]
         # condition = fd.load_condition(plate)
-        if remove_censored_events:
+
+        if exclude_censored_events and not ignore_subsequent_uncensored_events:
+            # We remove censored visits BEFORE taking the first
+            # visit. This means that later analysis will take the first uncensored visit.
             current_visits = fd.load_list(current_plate, "uncensored_visits")
+
+        if exclude_censored_events and ignore_subsequent_uncensored_events:
+            # What we do here is a bit dirty because extracting first visit is also done later,
+            # but at least we get it over with. We remove all visits to a patch if the first visit to this patch
+            # was censored.
+            all_visits = fd.load_list(current_plate, "no_hole_visits")
+            uncensored_visits = fd.load_list(current_plate, "uncensored_visits")
+            # Use a more convenient visit format, where they are sorted by patch instead of chronologically
+            all_visits = gr.sort_visits_by_patch(all_visits, param.nb_to_nb_of_patches[condition])
+            for i_patch in range(param.nb_to_nb_of_patches[condition]):
+                if len(all_visits[i_patch]) > 0:
+                    if all_visits[i_patch][0] + [i_patch] not in uncensored_visits:
+                        all_visits[i_patch] = []
+            current_visits = gr.sort_visits_chronologically(all_visits)
+
         else:
             current_visits = fd.load_list(current_plate, "no_hole_visits")
 
@@ -33,18 +63,22 @@ def bar_plot_first_visit_each_patch(results, condition_list, is_plot=True, remov
         if len(current_visits) > 0:
             if only_first_visited_patch:
                 first_visited_patch = current_visits[0][2]
-                for visit in current_visits:
+                original_visits = copy.deepcopy(current_visits)  # Making a copy for proper removal behavior!
+                for visit in original_visits:
                     if visit[2] != first_visited_patch:
                         current_visits.remove(visit)
             if soft_cut:
-                for visit in current_visits:
-                    if param.times_to_cut_videos[0] > visit[0] > param.times_to_cut_videos[1]:
+                original_visits = copy.deepcopy(current_visits)  # Making a copy for proper removal behavior!
+                for visit in original_visits:
+                    if (param.times_to_cut_videos[0] > visit[0]) or (visit[0] > param.times_to_cut_videos[1]):
                         current_visits.remove(visit)
             if hard_cut:
                 print("WARNING, YOU HAVENT IMPLEMENTED THE FULL HARD CUT FOR FIRST VISITS è_é")
-                for visit in current_visits:
-                    if visit[1] > param.time_to_cut_videos:
+                original_visits = copy.deepcopy(current_visits)  # Making a copy for proper removal behavior!
+                for visit in original_visits:
+                    if visit[1] > param.time_to_cut_videos[1]:
                         current_visits.remove(visit)
+
         # Only select the first visits
         list_of_found_patches = []
         first_value_each_patch = []
@@ -55,6 +89,7 @@ def bar_plot_first_visit_each_patch(results, condition_list, is_plot=True, remov
         if condition in condition_list and len(first_value_each_patch) > 0:
             condition_index = np.where(condition_list == condition)[0][0]
             avg_value_each_condition_each_plate[condition_index].append(np.nanmean(first_value_each_patch))
+
     for i_condition in range(len(condition_list)):
         avg_value_each_condition[i_condition] = np.mean(avg_value_each_condition_each_plate[i_condition])
         errors = ana.bottestrop_ci(avg_value_each_condition_each_plate[i_condition], 1000)
