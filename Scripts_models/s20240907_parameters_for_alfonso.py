ea91903a1b734af6849f13da0e89e68d0e700b61
martees <asmar.contact@yahoo.com>
2026-02-19 15:23:49 +0900

    minor debug

--- Scripts_models/s20240907_parameters_for_alfonso.py
+++ Scripts_models/s20240907_parameters_for_alfonso.py
@@ -6,6 +6,7 @@ from Generating_data_tables import main as gen
 import find_data as fd
 from Parameters import parameters as param
 import analysis as ana
+from Parameters.parameters import name_to_nb
 
 # Script to return the stuff that Alfonso wants for his model
 # For now, average total time spent outside per food patch, and average nb of visits per food patch, for each condition
@@ -33,61 +34,63 @@ for i_condition, condition_name in enumerate(condition_names):
     nb_of_visits_this_cond = []
     folder_list = fd.return_folders_condition_list(full_folder_list, param.name_to_nb_list[condition_name])
     for i_folder, folder in enumerate(folder_list):
-        # print(">>> Folder ", folder)
+        print(">>> Folder ", i_folder, "/", len(folder_list))
         current_results = results[results["folder"] == folder]
         current_visits = fd.load_list(current_results, "no_hole_visits")
         current_transits = fd.load_list(current_results, "aggregated_raw_transits")
+        current_visits, current_transits = ana.hard_cut_events(current_visits, current_transits)
 
-        # Only keep the first param.time_to_cut tracked time steps of the video
-        # Mix visits and transits and sort them by beginning
-        current_events = copy.deepcopy(current_visits) + copy.deepcopy(current_transits)
-        current_events = sorted(current_events, key=lambda x: x[0])
-        # Loop through them and stop when it's reached time to cut!!!
-        cumulated_duration_of_events = 0
-        i_event = 0
-        new_list_of_events = []
-        first_event_found = False
-        while i_event < len(current_events) and cumulated_duration_of_events < (
-                param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
-            current_event = current_events[i_event]
-            if current_event[0] >= param.times_to_cut_videos[0]:
-                if not first_event_found:
-                    first_event_found = True
-                    # If this is the first event that starts after time_to_cut[0] and not the first of the video, add the previous one
-                    if i_event > 0:
-                        previous_event = current_events[i_event - 1]
-                        previous_event[0] = param.times_to_cut_videos[0]  # but set it to start at time_to_cut[0]
-                        cumulated_duration_of_events += previous_event[1] - previous_event[0]
-                        new_list_of_events.append(previous_event)
-                        # If this previous event does not exceed time_to_cut[1], then you can add the current one
-                        if cumulated_duration_of_events < (param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
+        if type(current_visits) is list and type(current_transits) is list:
+            # Only keep the first param.time_to_cut tracked time steps of the video
+            # Mix visits and transits and sort them by beginning
+            current_events = copy.deepcopy(current_visits) + copy.deepcopy(current_transits)
+            current_events = sorted(current_events, key=lambda x: x[0])
+            # Loop through them and stop when it's reached time to cut!!!
+            cumulated_duration_of_events = 0
+            i_event = 0
+            new_list_of_events = []
+            first_event_found = False
+            while i_event < len(current_events) and cumulated_duration_of_events < (
+                    param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
+                current_event = current_events[i_event]
+                if current_event[0] >= param.times_to_cut_videos[0]:
+                    if not first_event_found:
+                        first_event_found = True
+                        # If this is the first event that starts after time_to_cut[0] and not the first of the video, add the previous one
+                        if i_event > 0:
+                            previous_event = current_events[i_event - 1]
+                            previous_event[0] = param.times_to_cut_videos[0]  # but set it to start at time_to_cut[0]
+                            cumulated_duration_of_events += previous_event[1] - previous_event[0]
+                            new_list_of_events.append(previous_event)
+                            # If this previous event does not exceed time_to_cut[1], then you can add the current one
+                            if cumulated_duration_of_events < (param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
+                                cumulated_duration_of_events += current_event[1] - current_event[0]
+                                new_list_of_events.append(current_event)
+                        # If this is the first event, just add it! and start it at time_to_cut[0]
+                        else:
+                            current_event[0] = param.times_to_cut_videos[0]  # but set it to start at time_to_cut[0]
                             cumulated_duration_of_events += current_event[1] - current_event[0]
                             new_list_of_events.append(current_event)
-                    # If this is the first event, just add it! and start it at time_to_cut[0]
+
                     else:
-                        current_event[0] = param.times_to_cut_videos[0]  # but set it to start at time_to_cut[0]
                         cumulated_duration_of_events += current_event[1] - current_event[0]
                         new_list_of_events.append(current_event)
+                i_event += 1
+            # In the end of the loop, if we have reached the cut parameter, cut the last event
+            if cumulated_duration_of_events > (param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
+                new_list_of_events[-1][1] -= cumulated_duration_of_events - (
+                            param.times_to_cut_videos[1] - param.times_to_cut_videos[0])
+            # Then, sort the events back to visits!
+            current_visits = [event for event in new_list_of_events if event[2] != -1]
 
-                else:
-                    cumulated_duration_of_events += current_event[1] - current_event[0]
-                    new_list_of_events.append(current_event)
-            i_event += 1
-        # In the end of the loop, if we have reached the cut parameter, cut the last event
-        if cumulated_duration_of_events > (param.times_to_cut_videos[1] - param.times_to_cut_videos[0]):
-            new_list_of_events[-1][1] -= cumulated_duration_of_events - (
-                        param.times_to_cut_videos[1] - param.times_to_cut_videos[0])
-        # Then, sort the events back to visits!
-        current_visits = [event for event in new_list_of_events if event[2] != -1]
-
-        total_time_out = np.sum(ana.convert_to_durations(current_transits))
-        nb_of_visits = len(current_visits)
-        if nb_of_visits > 0:  # if there's any visit
-            nb_of_visited_patches = len(np.unique(np.array(current_visits)[:, 2]))
-            # Fill the tablezzz
-            times_out_this_cond.append(total_time_out/nb_of_visited_patches)
-            nb_of_visits_this_cond.append(nb_of_visits/nb_of_visited_patches)
-            # print("Total time out: ", total_time_out, ", Nb of visited patches: ", nb_of_visited_patches, ", Effective time out: ", total_time_out / nb_of_visited_patches)
+            total_time_out = np.sum(ana.convert_to_durations(current_transits))
+            nb_of_visits = len(current_visits)
+            if nb_of_visits > 0:  # if there's any visit
+                nb_of_visited_patches = len(np.unique(np.array(current_visits)[:, 2]))
+                # Fill the tablezzz
+                times_out_this_cond.append(total_time_out/nb_of_visited_patches)
+                nb_of_visits_this_cond.append(nb_of_visits/nb_of_visited_patches)
+                # print("Total time out: ", total_time_out, ", Nb of visited patches: ", nb_of_visited_patches, ", Effective time out: ", total_time_out / nb_of_visited_patches)
 
     # Average and bootstrap times out per visited patch
     times_out_this_cond = np.array(times_out_this_cond)/3600  # convert to hours
